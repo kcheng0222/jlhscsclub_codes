7322df3d2922f6ba4dd24a16abbbb1ce64e6edd9
Kyle Cheng <37385704+kcheng0222@users.noreply.github.com>
2019-03-08 12:40:19 -0800

    Create all_possible_pairs.py

---
+++ 3_7_2019/all_possible_pairs.py
@@ -0,0 +1,14 @@
+#in recursion
+#not done
+
+arr = [1, 2, 3, 4, 5]
+
+def s(it, count, curr, arr):
+	if count == 4:
+		print(curr)
+	for i in range(it, n):
+		sum(i+1, count + 1, curr + arr[i], arr)
+		
+	
+	
+print(s(0, len(arr), 0, arr)
